488f1ab30e4270f98dd05f4166354f3297d83d96
westleya <31316123+westleya@users.noreply.github.com>
2017-11-17 10:34:35 -0700

    Add files via upload

---
+++ SortUtil.java
@@ -0,0 +1,27 @@
+package assignment04;
+
+public class SortUtil<T> {
+  public static <T> void mergesort(ArrayList<T>, Comparator<? super T>) {
+    
+  }
+
+
+  public static ArrayList<Integer> generateBestCase(int size) {
+    
+  }
+
+  public static ArrayList<Integer> generateAverageCase(int size){
+    
+  }
+
+
+  public static ArrayList<Integer> generateWorstCase(int size){
+    
+  }
+  /////////////////////////////////////////////////
+  ////////////////////QUICK SORT///////////////////
+  /////////////////////////////////////////////////
+  public static <T> void quicksort(ArrayList<T>, Comparator<? super T>) {
+    
+  }
+}
