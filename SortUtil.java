e2422afee30701710f6762fb2f8299ad2b107fce
Greg <skitch04@gmail.com>
2017-11-17 11:47:00 -0700

    added names to initial input variables

--- SortUtil.java
+++ SortUtil.java
@@ -1,27 +1,33 @@
 package assignment04;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class SortUtil<T> {
-  public static <T> void mergesort(ArrayList<T>, Comparator<? super T>) {
+  public static <T> void mergesort(ArrayList<T> inputList, Comparator<? super T> c) {
     
   }
 
 
   public static ArrayList<Integer> generateBestCase(int size) {
+    return null;
     
   }
 
   public static ArrayList<Integer> generateAverageCase(int size){
+    return null;
     
   }
 
 
   public static ArrayList<Integer> generateWorstCase(int size){
+    return null;
     
   }
   /////////////////////////////////////////////////
   ////////////////////QUICK SORT///////////////////
   /////////////////////////////////////////////////
-  public static <T> void quicksort(ArrayList<T>, Comparator<? super T>) {
+  public static <T> void quicksort(ArrayList<T> inputList, Comparator<? super T> c) {
     
   }
 }
