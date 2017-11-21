a7a7b8dd5cd355bfb352941dc155015dc663924e
Greg <skitch04@gmail.com>
2017-11-21 10:08:20 -0700

    working version of quick sort
    this is a slightly tested and working version of test sort. it only has one pivot selection method.

--- SortUtil.java
+++ SortUtil.java
@@ -4,114 +4,112 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class SortUtil<T> {
-  
-  private static int threshold = 10;
-  
-  public static <T> void mergesort(ArrayList<T> arr, Comparator<? super T> comp) {
-    ArrayList<T> temp = new ArrayList<T>(arr.size());
-    for (int i = 0; i < temp.size(); i++) {
-      temp.set(i, null);
-    }
-    mergesort(arr, temp, comp, 0, arr.size() - 1);
+  public static <T> void mergesort(ArrayList<T> inputList, Comparator<? super T> c) {
+    
   }
 
-  public static <T> void mergesort(ArrayList<T> arr, ArrayList<T> temp, 
-      Comparator<? super T> comp, int left, int right) {
-    
-    if ((right - left) > threshold) {
-      int center = (left + right) / 2;
-      mergesort(arr, temp, comp, left, center);
-      mergesort(arr, temp, comp, center + 1, right);
-      merge(arr, temp, comp, left, center + 1, right);
-    }
-    else {
-      insertionSort(arr, comp, left, right);
-    }
-  }
-  
-  public static <T> void merge(ArrayList<T> arr, ArrayList<T> temp, 
-      Comparator<? super T> comp, int left, int center_plus_one, int right) {
-    int temp_counter = left;
-    int lhc = left; // left-hand counter
-    int rhc = center_plus_one; // right-hand counter
-    while (temp_counter <= right) {
-      if (rhc > right) {
-        temp.set(temp_counter, arr.get(lhc));
-        temp_counter++; lhc++;
-        continue;
-      }
-      else if (lhc == center_plus_one) {
-        temp.set(temp_counter, arr.get(rhc));
-        temp_counter++; rhc++;
-        continue;
-      }
-      else if (comp.compare(arr.get(lhc), arr.get(rhc)) > 0) {
-        temp.set(temp_counter, arr.get(rhc));
-        temp_counter++; rhc++;
-        continue;
-      }
-      else {
-        temp.set(temp_counter, arr.get(lhc));
-        temp_counter++; lhc++;
-        continue;
-      }
-    }
-    
-    for (int i = 0; i < temp.size(); i++) {
-      if(!temp.get(i).equals(null)) {
-        arr.set(i, temp.get(i));
-      }
-    }
-  }
 
   public static ArrayList<Integer> generateBestCase(int size) {
-    ArrayList<Integer> best_arr = new ArrayList<Integer>();
-    for (int i = 0; i < size; i++) {
-      best_arr.add(i);
-    }
-    return best_arr;
+    return null;
+    
   }
 
   public static ArrayList<Integer> generateAverageCase(int size){
     return null;
+    
   }
 
 
   public static ArrayList<Integer> generateWorstCase(int size){
-    ArrayList<Integer> worst_arr = new ArrayList<Integer>();
-    for (int i = size; i > 0; i--) {
-      worst_arr.add(i);
-    }
-    return worst_arr;  }
-  
-  /** 
-   * InsertionSort sorts the input array using an insertion 
-   * sort algorithm and the input Comparator object. It assumes
-   * the array contains strings (words) and, for the purposes of
-   * finding anagrams, that the words themselves have been 
-   * sorted. (alphabetized)
-   * @param arr - a list of words
-   * @param comp - a comparator that determines how the words are sorted
-   * @return void - alphabetizes the list directly
-   */
-  public static <T> void insertionSort(ArrayList<T> arr, 
-      Comparator<? super T> comp, int left, int right) {
+    return null;
     
-    for(int i = left; i < right; i++){ // Sort the list 
-      T index = arr.get(i); 
-      int j = i;
-      while(j > left && (comp.compare(arr.get(j-1), index) > 0)){ 
-        // Swap everything until the proper insertion point is found
-        arr.add(j + 1, arr.get(j-1)); 
-        arr.remove(j-1);
-        j--;
-      }
-    }
   }
   /////////////////////////////////////////////////
   ////////////////////QUICK SORT///////////////////
   /////////////////////////////////////////////////
-  public static <T> void quicksort(ArrayList<T> arr, Comparator<? super T> comp) {
-    
+  public static <T> void quicksort(ArrayList<T> inputList, Comparator<? super T> c) {
+    quickSortRecursive(inputList, 0, inputList.size()-1, c);
   }
+  
+  private static <T> void quickSortRecursive(ArrayList<T> inputList, int startIndex, int endIndex, Comparator<? super T> c) {
+    if(startIndex >= endIndex) {
+      return;
+    }
+    //pick pivot
+    //int pivot = ((int)(Math.random() * 100))%(inputList.size());//pick a random pivot
+    int pivot = (endIndex - startIndex)/2 + startIndex;
+    pivot = partition(inputList, startIndex, endIndex, c, pivot);
+    quickSortRecursive(inputList, startIndex, pivot-1, c);
+    quickSortRecursive(inputList, pivot, endIndex, c);
+  }
+  
+  private static <T> int partition(ArrayList<T> inputList, int leftPoint, int rightPoint, Comparator<? super T> c, int pivotIndex) {
+   
+    while(leftPoint < rightPoint) {
+      while(c.compare(inputList.get(leftPoint), inputList.get(pivotIndex)) <= 0 && leftPoint < rightPoint) {
+        leftPoint++;
+        if((leftPoint==rightPoint) && (c.compare(inputList.get(leftPoint), inputList.get(pivotIndex))<=0)){
+          swap(inputList, leftPoint, pivotIndex);
+          return leftPoint;
+        }
+      }
+      
+      while(c.compare(inputList.get(rightPoint), inputList.get(pivotIndex)) >= 0) {
+        if(c.compare(inputList.get(leftPoint), inputList.get(rightPoint)) == 0 && rightPoint==leftPoint || rightPoint==0){
+          break;
+        }
+        rightPoint--;
+      }
+  
+      if(leftPoint < rightPoint) {
+        swap(inputList, leftPoint, rightPoint);
+        leftPoint ++;
+        rightPoint--;
+      }
+      
+    }
+    if(pivotIndex > leftPoint) {
+      T temp = inputList.get(pivotIndex);
+      shiftElements(inputList, leftPoint, pivotIndex);
+      inputList.set(leftPoint, temp);
+      return leftPoint;
+    }else {
+      return leftPoint;
+    }
+
+  }
+
+
+  /**
+   * @param inputList
+   * @param leftPoint
+   * @param rightPoint
+   */
+  private static <T> void swap(ArrayList<T> inputList, int leftPoint, int rightPoint) {
+    T temp = inputList.get(leftPoint);
+    inputList.set(leftPoint, inputList.get(rightPoint));
+    inputList.set(rightPoint, temp);
+  }
+
+  /**
+   * This method shifts all of the elements in the input list between the
+   *  startPos and endPos up ONE position. This results in the start element
+   *   appearing twice in the array and the endPos element getting overwritten
+   *   completely.
+   * @param inputList - arrayList of type T that will have a chunk of the elements shifted.
+   * @param startPos - the index of the first position to shift up.
+   * @param endPos - the index of the last elememt. 
+   *                  THIS ELEMENT WILL BE OVERWRITTEN!!!
+   */
+    private static <T> void shiftElements(ArrayList<T> inputList, int startPos, int endPos) {
+      if(endPos<startPos) {
+        for(int i = endPos; i < startPos; i++) {
+          inputList.set(i, inputList.get(i+1));
+        }
+      }
+      for(int i = endPos; i > startPos; i--) {
+        inputList.set(i, inputList.get(i-1));
+      }
+    }
+  
 }
