230e1a60fb9371b1d85bbfdade3cfcd3f1a1231e
westleya <31316123+westleya@users.noreply.github.com>
2017-11-21 10:14:28 -0700

    Add files via upload
    Started quicksort tests and combined code for QuickSort and merge sort.

--- SortUtil.java
+++ SortUtil.java
@@ -2,27 +2,157 @@ package assignment04;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Random;
 
 public class SortUtil<T> {
-  public static <T> void mergesort(ArrayList<T> inputList, Comparator<? super T> c) {
-    
+  
+  private static int threshold = 10; // When mergesort switches over to insertionsort
+  
+  /**
+   * Driver for mergesort algorithm. Creates temporary array to aid in sorting.
+   * @param arr - arraylist, of any type, that needs to 
+   * be sorted. Provided by the user.
+   * @param comp - comparator to use in sorting the items in arr.
+   */
+  public static <T> void mergesort(ArrayList<T> arr, Comparator<? super T> comp) {
+    ArrayList<T> temp = new ArrayList<T>(arr.size());
+    for (int i = 0; i < temp.size(); i++) {
+      temp.set(i, null);
+    }
+    mergesortRecursive(arr, temp, comp, 0, arr.size() - 1);
   }
 
-
+  /**
+   * The mergesort recursion. Splits the array until it reaches a certain threshold
+   * size and then switches out to insertion sort. 
+   * @param arr - list that needs to be sorted.
+   * @param temp - empty list to aid in sorting.
+   * @param comp - comparator used to determine ordering of values.
+   * @param left - low bound of current section of list.
+   * @param right - high bound of current section of list.
+   */
+  public static <T> void mergesortRecursive(ArrayList<T> arr, ArrayList<T> temp, 
+      Comparator<? super T> comp, int left, int right) {
+    
+    if ((right - left) > threshold) {
+      int center = (left + right) / 2;
+      mergesortRecursive(arr, temp, comp, left, center);
+      mergesortRecursive(arr, temp, comp, center + 1, right);
+      merge(arr, temp, comp, left, center + 1, right);
+    }
+    else {
+      insertionSort(arr, comp, left, right);
+    }
+  }
+  
+  /**
+   * Merges two sorted list sections using temp. The two sections must be adjacent.
+   * The sorted region in temp then replaces the equivalent slots in the original
+   * list, arr.
+   * @param arr - original list, requires sorting.
+   * @param temp - temporary list, contains one sorted section. (rest null)
+   * @param comp - comparator to determine ordering for the list.
+   * @param left - beginning of first sorted portion.
+   * @param center_plus_one - beginning of second sorted portion. 
+   * (one past the end of the first portion)
+   * @param right - end of second portion.
+   */
+  public static <T> void merge(ArrayList<T> arr, ArrayList<T> temp, 
+      Comparator<? super T> comp, int left, int center_plus_one, int right) {
+    int temp_counter = left;
+    int lhc = left; // left-hand counter
+    int rhc = center_plus_one; // right-hand counter
+    while (temp_counter <= right) {
+      if (rhc > right) {
+        temp.set(temp_counter, arr.get(lhc));
+        temp_counter++; lhc++;
+        continue;
+      }
+      else if (lhc == center_plus_one) {
+        temp.set(temp_counter, arr.get(rhc));
+        temp_counter++; rhc++;
+        continue;
+      }
+      else if (comp.compare(arr.get(lhc), arr.get(rhc)) > 0) {
+        temp.set(temp_counter, arr.get(rhc));
+        temp_counter++; rhc++;
+        continue;
+      }
+      else {
+        temp.set(temp_counter, arr.get(lhc));
+        temp_counter++; lhc++;
+        continue;
+      }
+    }
+    
+    for (int i = 0; i < temp.size(); i++) {
+      if(!temp.get(i).equals(null)) {
+        arr.set(i, temp.get(i));
+      }
+    }
+  }
+  
+  /**
+   * Creates an arraylist of ordered integers from 0 to size.
+   * @param size - the size of the list.
+   * @return - a new ordered list.
+   */
   public static ArrayList<Integer> generateBestCase(int size) {
-    return null;
-    
+    ArrayList<Integer> best_arr = new ArrayList<Integer>();
+    for (int i = 0; i < size; i++) {
+      best_arr.add(i);
+    }
+    return best_arr;
+  }
+  /**
+   * Creates a pseudorandom arraylist of the size size.
+   * @param size - the size of the list.
+   * @return - a new pseudorandom list.
+   */
+  public static ArrayList<Integer> generateAverageCase(int size) {
+    ArrayList<Integer> avg_arr = new ArrayList<Integer>();
+    Random rand = new Random();
+    rand.setSeed(2732);
+    for (int i = 0; i < size; i++) {
+      avg_arr.add(rand.nextInt());
+    }
+    return avg_arr;
   }
 
-  public static ArrayList<Integer> generateAverageCase(int size){
-    return null;
+  /**
+   * Creates a reverse-ordered arraylist of size size.
+   * @param size - the size of the list.
+   * @return - a new reverse-ordered list.
+   */
+  public static ArrayList<Integer> generateWorstCase(int size) {
+    ArrayList<Integer> worst_arr = new ArrayList<Integer>();
+    for (int i = size; i > 0; i--) {
+      worst_arr.add(i);
+    }
+    return worst_arr;  }
+  
+  /** 
+   * InsertionSort sorts the input array using an insertion 
+   * sort algorithm and the input Comparator object. It assumes
+   * the array contains strings (words) and, for the purposes of
+   * finding anagrams, that the words themselves have been 
+   * sorted. (alphabetized)
+   * @param arr - a list of words
+   * @param comp - a comparator that determines how the words are sorted
+   * @return void - alphabetizes the list in place, no return.
+   */
+  public static <T> void insertionSort(ArrayList<T> arr, 
+      Comparator<? super T> comp, int left, int right) {
     
-  }
-
-
-  public static ArrayList<Integer> generateWorstCase(int size){
-    return null;
-    
+    for(int i = left; i < right; i++){ // Sort the list 
+      T index = arr.get(i); 
+      int j = i;
+      while(j > left && (comp.compare(arr.get(j-1), index) > 0)){ 
+        // Swap everything until the proper insertion point is found
+        swap(arr, j - 1, j);
+        j--;
+      }
+    }
   }
   /////////////////////////////////////////////////
   ////////////////////QUICK SORT///////////////////
@@ -78,19 +208,19 @@ public class SortUtil<T> {
     }
 
   }
-
-
+  
   /**
-   * @param inputList
-   * @param leftPoint
-   * @param rightPoint
+   * Switches adjacent values in the inputList.
+   * @param inputList - arraylist containing values to be swapped.
+   * @param leftPoint - location of first value.
+   * @param rightPoint - location of second value.
    */
   private static <T> void swap(ArrayList<T> inputList, int leftPoint, int rightPoint) {
     T temp = inputList.get(leftPoint);
     inputList.set(leftPoint, inputList.get(rightPoint));
     inputList.set(rightPoint, temp);
   }
-
+  
   /**
    * This method shifts all of the elements in the input list between the
    *  startPos and endPos up ONE position. This results in the start element
@@ -111,5 +241,4 @@ public class SortUtil<T> {
         inputList.set(i, inputList.get(i-1));
       }
     }
-  
 }
